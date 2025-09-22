5520246d20aae9178cc6241d134a622135cbf575
Itapon <iz.tapon.foruria@gmail.com>
2025-09-22 20:29:37 +0200

    Add files via upload
    f

---
+++ libft/ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itapon <itapon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/21 21:05:49 by itapon            #+#    #+#             */
+/*   Updated: 2025/09/22 20:11:43 by itapon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+
+	if (d > s)
+	{
+		while(i < n)
+		{
+			ptr[i] = (char)c;
+			i++;
+		}
+	}
+	return (*dest);
+}
+
+int main (void)
+{
+	size_t n;
+	char	src[] = "vaquita";
+	char	dest[];
+
+	ft_memmove();
+}
